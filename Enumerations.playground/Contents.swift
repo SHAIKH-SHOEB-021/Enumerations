c3c418459cb3c0986ebafdd927594c59cd2fe235
vinaygupta.cont <vinaygupta.cont@niveussolutions.com>
2022-12-19 11:21:37 +0530

    Initial Commit

---
+++ Enumerations.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+enum PizzaSize {
+  case small, medium, large
+}
+
+var size = PizzaSize.medium
+
+switch(size) {
+  case .small:
+    print("I ordered a small size pizza.")
+
+  case .medium:
+    print("I ordered a medium size pizza.")
+
+   case .large:
+     print("I ordered a large size pizza.");
+}
